4dc53b411686efacedabd6fd8701957515eec780
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2022-05-17 19:56:50 -0300

    Add a User Interface (CLI)

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from src.classes import Bill, Flatmate, PdfReport
 
-the_bill = Bill(amount=120, period='March 2022')
-jhonny = Flatmate(name="Jhonny", days_in_house=20)
-mary = Flatmate(name="Mary", days_in_house=25)
+amount = float(input('Enter the bill amount: '))
+period = input('What is the bill period ? E.g. December 2020: ')
 
-print("Jhonny pays: ", jhonny.pays(bill=the_bill, flatmate2=mary))
-print("Mary pays: ", mary.pays(bill=the_bill, flatmate2=jhonny))
+flatmate1_name = input('What is your name ? ')
+flatmate1_days_in_house = int(input(f"How many days did {flatmate1_name} stay in the house during teh bill period ? "))
+
+flatmate2_name = input('Who is the other flatmate ? ')
+flatmate2_days_in_house = int(input(f"How many days did {flatmate2_name} stay in the house during teh bill period ? "))
+
+the_bill = Bill(amount=amount, period=period)
+flatmate1 = Flatmate(name=flatmate1_name, days_in_house=flatmate1_days_in_house)
+flatmate2 = Flatmate(name=flatmate2_name, days_in_house=flatmate2_days_in_house)
+
+print(f"{flatmate1.name} pays: ", flatmate1.pays(bill=the_bill, flatmate2=flatmate2))
+print(f"{flatmate2.name} pays: ", flatmate2.pays(bill=the_bill, flatmate2=flatmate1))
 
 pdf_report = PdfReport(filename="Report.pdf")
-pdf_report.generate(flatmate1=jhonny, flatmate2=mary, bill=the_bill)
+pdf_report.generate(flatmate1=flatmate1, flatmate2=flatmate2, bill=the_bill)
