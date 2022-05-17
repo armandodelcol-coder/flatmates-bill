b212633bebbed54595d2bf86f19e0129a973eb85
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2022-05-16 22:10:40 -0300

    Implementing the pays method

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from src.classes import Bill, Flatmate
 
-the_bill = Bill(amount=250, period='March 2022')
+the_bill = Bill(amount=120, period='March 2022')
 jhonny = Flatmate(name="Jhonny", days_in_house=20)
 mary = Flatmate(name="Mary", days_in_house=25)
 
-print(jhonny.pays(bill=the_bill))
+print("Jhonny pays: ", jhonny.pays(bill=the_bill, flatmate2=mary))
+print("Mary pays: ", mary.pays(bill=the_bill, flatmate2=jhonny))
--- src/classes.py
+++ src/classes.py
@@ -19,8 +19,10 @@ class Flatmate:
         self.days_in_house = days_in_house
         self.name = name
 
-    def pays(self, bill: Bill):
-        return 'Not implemented yet'
+    def pays(self, bill: Bill, flatmate2: "Flatmate"):
+        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
+        to_pay = bill.amount * weight
+        return to_pay
 
 
 class PdfReport:
